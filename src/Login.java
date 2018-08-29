957524c1c4e5cc994f54d5d6262a001608be9583
Jitrat <59160931@go.buu.ac.th>
2018-08-29 23:11:04 +0700

    First Commit to Git

---
+++ src/Login.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login {
+    private JTextField txtUserLogin;
+    private JPasswordField pwdUserPssword;
+    private JButton btnSubmit;
+    private JButton btnClear;
+    private JPanel mainPanel;
+
+    public Login() {
+        btnSubmit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, txtUserLogin.getText() + " " + new String(pwdUserPssword.getPassword()));
+            }
+        });
+
+        btnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtUserLogin.setText("");
+                pwdUserPssword.setText("");
+            }
+        });
+    }
+
+        public JPanel getMainPanel(){
+            return mainPanel;
+        }
+    }
+
